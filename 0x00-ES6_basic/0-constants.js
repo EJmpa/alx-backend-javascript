06c5e3627823c4777932627bf6afa0344d70e578
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-09-29 22:04:38 +0000

    const & let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+// 0-constants.js
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.'; // Use const for variables that don't change
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let'; // Use let for variables that can change
+  combination += getLast();
+
+  return combination;
+}
