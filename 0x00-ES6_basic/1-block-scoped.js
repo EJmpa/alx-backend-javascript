96a2c63bfa51787e3dc4ea15eb5556ded0eca014
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-01 19:37:50 +0000

    task1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
+
