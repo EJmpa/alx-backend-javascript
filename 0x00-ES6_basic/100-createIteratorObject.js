b977894009509b058e2d4a796c6d5d5a5b07a298
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-02 16:33:12 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,9 +2,11 @@ export default function createIteratorObject(report) {
   return {
     *[Symbol.iterator]() {
       for (const department in report.allEmployees) {
-        const employees = report.allEmployees[department];
-        for (const employee of employees) {
-          yield employee;
+        if (report.allEmployees.hasOwnProperty(department)) {
+          const employees = report.allEmployees[department];
+          for (const employee of employees) {
+            yield employee;
+          }
         }
       }
     },
