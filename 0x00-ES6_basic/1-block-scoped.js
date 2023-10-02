16714ad66ab2d5b8edea003bce12edb864a2dd7a
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-02 13:24:24 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
-/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;   // Inner const task shadows the outer task
-    const task2 = false; // Inner const task2 shadows the outer task2
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
 }
-/* eslint-enable no-unused-vars */
