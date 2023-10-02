28455fe6377751b3c01b76c24162a1257b9521f5
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-02 13:39:31 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Use const for variables that don't change
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,9 +8,8 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let'; // Use let for variables that can change
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 }
-
