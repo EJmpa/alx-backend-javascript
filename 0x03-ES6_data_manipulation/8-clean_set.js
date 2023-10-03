642c9d978caa9d7bb331c3df35eff8f7327b27f0
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-03 20:33:09 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,16 @@
 function cleanSet(set, startString) {
+  if (startString === '') {
+    return null; // You can return any appropriate value, such as null or an empty string.
+  }
+
   // Use the spread operator (...) to convert the Set to an array.
   const setArray = [...set];
 
   // Use filter() to keep only the values that start with the specified startString.
-  const filteredArray = setArray.filter((value) => value.startsWith(startString));
+  const filteredArray = setArray.filter(value => value.startsWith(startString));
 
   // Use map() to extract the part of each value that comes after startString.
-  const cleanedArray = filteredArray.map((value) => value.substring(startString.length));
+  const cleanedArray = filteredArray.map(value => value.substring(startString.length));
 
   // Use join() to concatenate the cleaned values with '-' as the separator.
   const resultString = cleanedArray.join('-');
