3e946942aac2469dc80a8775a4221b3bc931c068
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-09 08:38:26 +0000

    typeScript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,4 +9,40 @@ interface Teacher {
 
 interface Directors extends Teacher {
   numberOfReports: number;
+  }
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  const initial = firstName.charAt(0); // Get the first letter of firstName
+  return `${initial}. ${lastName}`;
+  };
+
+// Task 4
+// Step 1: Create an Interface for the Constructor
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Step 2: Create the StudentClass Class
+class StudentClass implements StudentConstructor {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
