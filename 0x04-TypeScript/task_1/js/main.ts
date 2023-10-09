4e03ba142edf5f774cb0db724160d4d474f52b86
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-09 07:57:27 +0000

    task_0

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; // Allow additional attributes with any type
+}
